12dd874d618bf973f3aa0a40509d5b2bb41535f5
Yoel Garcia Diaz <ygarciadiaz@bluecatnetworks.com>
2022-04-04 10:40:18 -0400

    fix typo

--- sorting.go
+++ sorting.go
@@ -6,10 +6,8 @@ func Sorted[O Ordering[S], S any](o O, arr []S) []S {
 		if len(a) <= 1 {
 			return a
 		}
-		first := make([]S, 0, 0)
-		second := make([]S, 0, 0)
-		first = append(first, a[:len(a)/2]...)
-		second = append(second, a[len(a)/2:]...)
+		first := append(make([]S, 0, 0), a[:len(a)/2]...)
+		second := append(make([]S, 0, 0), a[len(a)/2:]...)
 		return MergeSorted(o, sort(first), sort(second))
 	}
 	return sort(arr)
@@ -21,8 +19,8 @@ func MergeSorted[O Ordering[S], S any](o O, arr1 []S, arr2 []S) []S {
 	} else if len(arr2) == 0 {
 		return arr1
 	} else {
-		a1 := append(make([]S, 0.0), arr1...)
-		a2 := append(make([]S, 0.0), arr2...)
+		a1 := append(make([]S, 0, 0), arr1...)
+		a2 := append(make([]S, 0, 0), arr2...)
 		if o.Lt(a1[0], a2[0]) || o.Equiv(a1[0], a2[0]) {
 			return append(a1[:1], MergeSorted(o, a1[1:], a2)...)
 		} else {
